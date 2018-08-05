faea999e1db8b75166dce377969a0d48627652fa
CesarArtezan <arteza.cabrera@gmail.com>
2018-08-05 18:43:50 -0500

    serve

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/bricksApp'));
+app.use(express.static('./www'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/bricksApp/index.html'));
+res.sendFile(path.join(__dirname,'/www/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(3001);
